afa061b385e6a046b7cd23fecbc017ce128c55bb
Etienne <phone.mfe@gmail.com>
2019-07-18 22:37:21 -0400

    fixed equalButton issue, fixed consecutive operations issue

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function functionButton(x){
         num2 = "";
         answer = "";
         displayValue.innerHTML = "0";
-        clickedEqualButton = 0;
+        equalButtonClicked = 0;
         operatorButtonClicked = 0;
     }else if (x === 2){
         if (displayValue.innerHTML > 0 || displayValue.innerHTML < 0){
@@ -53,6 +53,11 @@ function numberButton(x){
 
 //OPERATOR BUTTONS
 function operatorButton(x){
+    //FIXED : moved this if statement to the top and solved following issue (power of order): > Purpose: to update the display with the answer to the previous operation when operator button is clicked for a third or more operations like it does on the iphone calculator --- Currently: only works with same operations (ie. add, substract, etc.), for other operations it performs the most recent operation instead of showing the result of the previous one. ex : "2 + 3 -" should show "5" so "5 - ?" would be the next operation, but currently shows "2 + 3 -" as "-1"
+    if (!num2 == ""){
+        equalButton();
+    }
+    
     if (x === '/'){
         operator = '/';
     }else if (x === '*'){
@@ -63,12 +68,8 @@ function operatorButton(x){
         operator = '+';
     }
     
-    //still needs tweaking --- Purpose: to update the display with the answer to the previous operation when operator button is clicked like it does on the iphone calculator --- Currently: only works with same operations (ie. add, substract, etc.), for other operations it performs the most recent operation instead of showing the result of the previous one. ex : "2 + 3 -" should show "5" so "5 - ?" would be the next operation, but currently shows "2 + 3 -" as "-1"
     operatorButtonClicked = 1;
-    if (!num2 == ""){
-        equalButton();
-    }
-
+    equalButtonClicked = 0;
 }
 
 //DECIMAL BUTTON
@@ -91,30 +92,57 @@ function decimalButton(x){
 }
 
 //EQUAL BUTTON
+//FIXED > if equal button is pressed more than once = NaN
 function equalButton(){
-    clickedEqualButton = 1;
-
-    let value1 = parseFloat(num1);
-    let value2 = parseFloat(num2);
-
     // console.log(value1);
     // console.log(operator);
     // console.log(value2);
 
-    if (operator === '/'){
-        answer = value1 / value2;
-		displayValue.innerHTML = answer;
-	}else if (operator === '*'){
-		answer = value1 * value2;
-		displayValue.innerHTML = answer;
-	}else if (operator === '-'){
-		answer = value1 - value2;
-		displayValue.innerHTML = answer;
-	}else if (operator === '+'){
-		answer = value1 + value2;
-		displayValue.innerHTML = answer;
-    }
+    calculate();
     
     num1 = answer;
     num2 = "";
+    equalButtonClicked = 1;
 }
+
+
+function calculate (){
+    let value1 = parseFloat(num1);
+    let value2 = parseFloat(num2);
+
+    if (equalButtonClicked === 0){
+        if (operator === '/'){
+            answer = value1 / value2;
+            displayValue.innerHTML = answer.toString().slice(0, 9);
+        }else if (operator === '*'){
+            answer = value1 * value2;
+            displayValue.innerHTML = answer.toString().slice(0, 9);
+        }else if (operator === '-'){
+            answer = value1 - value2;
+            displayValue.innerHTML = answer.toString().slice(0, 9);
+        }else if (operator === '+'){
+            answer = value1 + value2;
+            displayValue.innerHTML = answer.toString().slice(0, 9);
+        }
+    } else {
+        displayValue.innerHTML = answer;
+    }
+}
+// the --> answer.toString().slice(0, 9); <-- solution is not satisfactory
+// Should create a greaterLesserThanNine 0s function for the answers or just an answer function? if solving for both greater and lesser in the same function would need to add to percent button click event. Should return something like 9e10 or 9e-10 --- possibly will need to use a for loop
+
+// function moreThanNineZerosRight () {
+//     //Should return something like 9e10
+//     if (answer > 0 && answer.length > 9){
+//         //for loop? to test for numbers greater than zero so can set decimal and evaluate number after 'e' --- ex: if 980,000,000 * 100 = 9.8e10 || 980,000,000 * 1,000 = 9.8e11
+//     }
+// }
+// function moreThanNineZerosLeft () {
+//     //Should return something like 9e-10
+//     if (answer < 0 && answer.length > 9){
+        
+//     }
+// }
+
+//Extra Features : 1) hilighted operator buttons, 2)Font size decreasing as amount of integers increase up to 9 characters
+//
